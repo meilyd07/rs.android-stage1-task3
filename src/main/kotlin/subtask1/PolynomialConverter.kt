f56f31570a2e3721948daefbfd596bba31800f00
MeilyD07 <meilyd07@gmail.com>
2020-05-02 00:02:00 +0300

    feat: add polynomial converter task

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,29 @@
 package subtask1
 
+import java.lang.StringBuilder
+import kotlin.math.abs
+
 class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        val countItems = numbers.count()
+        if (countItems == 0) return null
+
+        var resultString: StringBuilder = StringBuilder()
+        for(i in 0 until countItems){
+            val currentInt = numbers[i]
+            val absCurrentInt = abs(currentInt)
+            val currentDegree = countItems - i - 1
+
+            if (currentInt != 0) {
+                resultString.append(if (currentInt > 0) (if (currentDegree == countItems - 1) "" else " + ") else (if (currentDegree == countItems - 1) "-" else " - "))
+                resultString.append(if ((currentDegree == 0) || ((currentDegree > 0) && (absCurrentInt != 1))) absCurrentInt else "")
+                resultString.append(if (currentDegree > 0) "x" else "")
+                resultString.append(if (currentDegree > 1) "^$currentDegree" else "")
+            }
+        }
+
+        return resultString.toString()
     }
 }
