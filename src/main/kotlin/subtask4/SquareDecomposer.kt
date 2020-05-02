bc3d243b3ee856765bb6fb599db3655381127044
MeilyD07 <meilyd07@gmail.com>
2020-05-02 15:58:30 +0300

    feat: add square decomposer task

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -4,6 +4,40 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val longNumber = number.toLong()
+        var decomposeArray = decomposeNumberAndRemaining(longNumber, longNumber*longNumber)
+
+        if (decomposeArray != null) {
+            decomposeArray.remove(decomposeArray.last())
+            val decomposeList = mutableListOf<Int>()
+
+            for(item in decomposeArray) {
+                val value = item.toInt()
+                decomposeList.add(value)
+            }
+            return decomposeList.toTypedArray()
+        }
+
+        return null
+    }
+
+        private fun decomposeNumberAndRemaining(current: Long, rest: Long) : MutableList<Long>? {
+            if (rest == 0L) {
+                val resultList: MutableList<Long> = mutableListOf<Long>()
+                resultList.add(current)
+                return resultList
+            }
+
+            //foreach number less than current
+            for (i in  (current - 1).toLong() downTo 0 step 1) {
+                if ((rest - i * i) >= 0) {
+                    val resultList = decomposeNumberAndRemaining(i, (rest - i * i))
+                    if (resultList != null) {
+                        resultList.add(current)
+                        return resultList
+                    }
+                }
+            }
+            return null
     }
 }
